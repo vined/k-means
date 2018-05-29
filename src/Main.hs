01c5d96c1d04d6c828e0ed7d0adbc40937e6c6be
Edvinas Naraveckas <e.naraveckas@gmail.com>
2018-05-29 23:06:34 +0300

    K-Means implementation

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,14 @@ module Main where
 import System.Environment (getArgs)
 import System.Random (getStdGen, randomRs)
 import System.IO (FilePath, readFile)
-import Data.List (map)
+import Data.List (find, groupBy, map, sortOn, zip)
+
+
+type Cls = Int
+
+
+getN :: [a] -> Int
+getN a = (length a) - 1
 
 
 splitOn :: Char -> String -> [String]
@@ -13,41 +20,153 @@ splitOn delim str =
         else (\(a, b) -> if length b < 1 then [str] else [a] ++ (splitOn delim (tail b))) (break (== delim) str)
 
 
-parseLine :: String -> [String]
-parseLine line = splitOn ',' line
+splitLine :: String -> [String]
+splitLine = splitOn ','
 
 
-readData :: FilePath -> IO [[String]]
-readData filePath = do
+-- getColMean :: [[Double]] -> Int -> Double
+-- getColMean rows idx
+
+
+-- replaceEmptyWithMean rows
+
+
+-- fillEmptyWithMean :: [[Double]] -> [[Double]]
+-- fillEmptyWithMean rows =
+--     let
+--         colsCnt = getN $ head rows
+--         means = map (getColMean cols) [0..colsCnt]
+--     in
+--         replaceEmptyWithMean rows means
+--
+--
+-- getColsData :: [[String]] -> [(Double)]
+
+
+selectCols :: [Int] -> [String] -> [String]
+selectCols usedCols cols =
+    map (\idx -> cols!!idx) usedCols
+
+
+parseRow :: [Int] -> [String] -> [Double]
+parseRow usedCols row =
+    map read $ selectCols usedCols row
+
+
+parseRows :: [Int] -> [[String]] -> [[Double]]
+parseRows usedCols rows =
+    map (parseRow usedCols) rows
+
+
+readData :: FilePath -> [Int] -> IO [[Double]]
+readData filePath usedCols = do
     content <- readFile filePath
-    return $ map parseLine $ lines content
+    return $ parseRows usedCols $ map splitLine $ lines content
+
+
+diff :: [Double] -> [Double] -> Double
+diff a b =
+    foldl (+) 0 $ map (\(ai, bi) -> abs (ai - bi)) $ zip a b
+
+
+assignCluster :: [(Cls, [Double])] -> [Double]-> (Cls, [Double])
+assignCluster centers cols =
+    let
+        diffs = map (\(cls, center) -> (cls, diff center cols)) centers
+        bestMatch = head $ sortOn snd diffs
+    in
+        (fst bestMatch, cols)
+
+
+remapToClusters :: [(Cls, [Double])] -> [[(Cls, [Double])]] -> [(Cls, [[Double]])]
+remapToClusters centers d =
+    let
+        newClusters = map (\cluster -> (fst $ head cluster, map snd cluster)) d
+    in
+        map (\cls -> (cls, maybe [] snd $ find (\x -> cls == fst x) newClusters)) $ map fst centers
+
+
+cluster :: [(Cls, [Double])] -> [[Double]]-> [(Cls, [[Double]])]
+cluster centers ds =
+    remapToClusters centers $ groupBy (\a b -> (fst a) == (fst b)) $ sortOn fst $ map (assignCluster centers) ds
+
+
+mean :: [Double] -> Double
+mean ds = sum ds / fromIntegral (length ds)
+
+
+getCenter :: [[Double]] -> [Double]
+getCenter = map mean
+
+
+getCenters :: [(Cls, [[Double]])] -> [(Cls, [Double])]
+getCenters = map (\(cls, ds) -> (cls, getCenter ds))
+
+
+getMaxDiff :: [(Cls, [Double])] -> [(Cls, [Double])] -> Double
+getMaxDiff newCenters oldCenters =
+    maximum $ map (\(a, b) -> diff a b) $ zipWith (\a b -> (snd a, snd b)) newCenters oldCenters
+
+
+isCloseEnough :: [(Cls, [Double])] -> [(Cls, [Double])] -> Bool
+isCloseEnough newCenters oldCenters = (getMaxDiff newCenters oldCenters) < 0.00001
+
+
+isEnough :: Int -> [(Cls, [Double])] -> [(Cls, [Double])] -> Bool
+isEnough iteration newCenters oldCenters =
+    iteration > 20 || isCloseEnough newCenters oldCenters
+
+
+kMeans :: Int -> [(Cls, [Double])] -> [[Double]] -> (Int, [(Cls, [Double])], [(Cls, [[Double]])])
+kMeans iteration centers ds =
+    let
+        clustered = cluster centers ds
+        newCenters = getCenters clustered
+    in
+        if isEnough iteration newCenters centers
+            then
+                (iteration, newCenters, clustered)
+            else
+                kMeans (iteration+1) newCenters ds
 
 
 main :: IO ()
 main = do
-    (fileName:clustersCntStr:ignoredColumnsStrs) <- getArgs
+    (fileName:clustersCntStr:usedColsStrs) <- getArgs
     putStrLn ""
     putStrLn "=== K-Means ==="
+
+    let clustersCnt = read clustersCntStr :: Int
+    let usedCols = map read usedColsStrs :: [Int]
+
+    putStrLn $ "Clusters count: " ++ (show clustersCnt)
+    putStrLn $ "Used columns: [" ++ (foldl (\acc c -> acc ++ (show c) ++ " ") " " usedCols) ++ "]"
 
     putStrLn ""
     putStrLn "-- Reading data --"
     putStrLn $ "File: " ++ fileName
-    d <- readData fileName
-    putStrLn $ "Dataset size: " ++ (show $ length d)
-    putStrLn $ "Columns count: " ++ (show $ length $ head d)
+
+    ds <- readData fileName usedCols
+    let dataSetSize = length ds
+
+    putStrLn $ "Dataset size: " ++ (show dataSetSize)
+    putStrLn $ "Columns count: " ++ (show $ length $ head ds)
 
     putStrLn ""
     putStrLn "-- Running K-Means --"
 
-    let clustersCnt = read clustersCntStr :: Int
-    let ignoredColumns = map read ignoredColumnsStrs :: [Int]
-    putStrLn $ "Clusters count: " ++ (show clustersCnt)
-    putStrLn $ "Ignored columns: " ++ (foldl (\acc c -> acc ++ (show c) ++ ", ") "" ignoredColumns)
-
-
-
-    let dataSize = 200
     g <- getStdGen
-    let initialCenters = take clustersCnt (randomRs (0, 200) g) :: [Int]
+    let initialCentersIdx = take clustersCnt (randomRs (0, dataSetSize - 1) g) :: [Int]
+    putStrLn $ "Initial centers: " ++ (foldl (\acc c -> acc ++ (show c) ++ ", ") "" initialCentersIdx)
 
-    putStrLn $ "Initial centers: " ++ (foldl (\acc c -> acc ++ (show c) ++ ", ") "" initialCenters)
+    let initialCenters = zip [0..clustersCnt-1] $ map (\idx -> ds!!idx) initialCentersIdx
+    let (iterations, centers, clusters) = kMeans 1 initialCenters ds
+
+    putStrLn $ "Iterations count: " ++ (show iterations)
+    putStrLn $ "Centers count: " ++ (show $ length centers)
+    putStrLn $ "Clusters count: " ++ (show $ length clusters)
+
+    -- TODO output results
+
+    putStrLn ""
+    putStrLn "Done."
